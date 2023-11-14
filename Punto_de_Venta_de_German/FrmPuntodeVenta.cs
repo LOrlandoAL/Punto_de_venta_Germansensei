f37e228505c5bb8e7163df4e4b104491f702944b
LUIS ORLANDO ANDRADE LEMUS <Ls21120218@surguanajuato.tecnm.mx>
2023-11-13 18:00:15 -0600

    Agregamos la base del codigo.

---
+++ Punto_de_Venta_de_German/FrmPuntodeVenta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto_de_Venta_de_German
+{
+    public partial class FrmPuntodeVenta : Form
+    {
+        public FrmPuntodeVenta()
+        {
+            InitializeComponent();
+        }
+    }
+}
