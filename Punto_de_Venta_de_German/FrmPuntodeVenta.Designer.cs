acda6a7ff2f97faf8dfec35424925877d70fb8fe
LUIS ORLANDO ANDRADE LEMUS <Ls21120218@surguanajuato.tecnm.mx>
2023-11-13 20:38:41 -0600

    Mas cosas.

--- Punto_de_Venta_de_German/FrmPuntodeVenta.Designer.cs
+++ Punto_de_Venta_de_German/FrmPuntodeVenta.Designer.cs
@@ -415,7 +415,7 @@
             this.panel2.Location = new System.Drawing.Point(361, 32);
             this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(421, 139);
+            this.panel2.Size = new System.Drawing.Size(420, 139);
             this.panel2.TabIndex = 12;
             // 
             // button2
@@ -457,7 +457,7 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.label9.Location = new System.Drawing.Point(23, 91);
+            this.label9.Location = new System.Drawing.Point(21, 52);
             this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(28, 16);
